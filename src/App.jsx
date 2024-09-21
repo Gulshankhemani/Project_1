fe9a5e6c2dbd261e4f6aa7c046d09880bbda6163
Gulshankhemani <gulshankhemani2@gamil.com>
2024-09-21 17:26:20 +0530

    Second_commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,21 @@ import Benifits from "./components/Benifits";
 import Collabration from "./components/Collabration";
 import Services from "./components/Services";
 import Pricing from "./components/Pricing";
+import Roadmap from "./components/Roadmap";
+import Footer from "./components/Footer"
 
 const App = () => {
   return (
     <>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-    <Header />
-    <Hero />
-    <Benifits />
-    <Collabration/>
-    <Services/>
-    <Pricing/>
+        <Header />
+        <Hero />
+        <Benifits />
+        <Collabration />
+        <Services />
+        <Pricing />
+        <Roadmap />
+        <Footer/>
       </div>
       <ButtonGradient />
     </>
